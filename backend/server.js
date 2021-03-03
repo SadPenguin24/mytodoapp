d080c17128ac3069a45e637ea98222f1db2afb21
unknown <danhendrix.pro@gmail.com>
2021-03-04 04:07:37 +0800

    refactor for production"

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-//import path from "path";
+import path from "path";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import listRoutes from "./routes/listRoutes.js";
@@ -15,9 +15,7 @@ app.use(express.json());
 
 app.use("/lists", listRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+const __dirname = path.resolve();
 
 //connect mongodb
 const uri = process.env.MONGO_URI;
@@ -31,10 +29,24 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(notFound);
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${port}`
+  );
 });
